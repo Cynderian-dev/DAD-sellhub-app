b140588f6e1a0995ec1417860b7ce3c5944f233c
Cynderian-dev <darkeragon8888@hotmail.com>
2022-02-03 12:08:55 +0100

    commit prueba cliente

--- src/main/java/es/urjccode/controllers/ClienteController.java
+++ src/main/java/es/urjccode/controllers/ClienteController.java
@@ -1,5 +1,18 @@
 package es.urjccode.controllers;
 
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
 public class ClienteController {
+	
+	@GetMapping("/cliente")
+	public String cliente(Model model) {
+		
+		model.addAttribute("name", "cliente");
+		
+		return "cliente";
+	}
 
 }
