a28e6482a8684fd3c27fbdcdbceb7237449cc020
Sergio Javier Sanchez Cambra <sj.sanchezc@alumnos.urjc.es>
2022-02-15 14:05:39 +0100

    añadido esqueleto OfertaController

--- src/main/java/es/urjccode/controllers/OfertaController.java
+++ src/main/java/es/urjccode/controllers/OfertaController.java
@@ -4,6 +4,7 @@ import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
 
 @Controller
 public class OfertaController {
@@ -28,5 +29,13 @@ public class OfertaController {
 		
 		return "template_crear_oferta";
 	}
+	
+	@PostMapping("/crear-oferta")
+	public String crearOferta(Model model) {
+		
+		
+		return "template_crear_oferta";
+	}
+
 
 }
