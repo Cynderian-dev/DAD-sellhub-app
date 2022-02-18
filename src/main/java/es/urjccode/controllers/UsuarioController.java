0bc4d0858656dde28d1c55a7ba31fd1e4ef93713
sergioj97 <sergiojsancam@gmail.com>
2022-02-18 23:54:47 +0100

    Modificado UsuarioController (id de usuarios en panel)
    Modificado UsuarioController para recibir un id de usuario del que
    mostrar información en la página de panel de usuario

--- src/main/java/es/urjccode/controllers/UsuarioController.java
+++ src/main/java/es/urjccode/controllers/UsuarioController.java
@@ -1,20 +1,42 @@
 package es.urjccode.controllers;
 
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+
+import es.urjccode.models.UsuarioModel;
+import es.urjccode.repositories.UsuarioRepo;
 
 @Controller
 public class UsuarioController {
 	
+	@Autowired
+	private UsuarioRepo usuarioRepo;
+	
 	@GetMapping("/panel-usuario")
-	public String mostrarPanelUsuario() {
+	public String mostrarPanelUsuarioPersonal(Model model) {
 		
+		// TODO: Sustituir usuario grabado a fuego por el usuario que está navegando la página
+		model.addAttribute("usuario_seleccionado", usuarioRepo.getById((long) 1));
+		return "template_panel_usuario";
+	}
+	
+	@GetMapping("/panel-usuario/{id}")
+	public String mostrarPanelUsuario(Model model, @PathVariable Long id) {
+		
+		model.addAttribute("usuario_seleccionado", usuarioRepo.getById(id));
 		return "template_panel_usuario";
 	}
 	
 	@GetMapping("/buscador-usuarios")
-	public String mostrarBuscadorUsuarios() {
+	public String mostrarBuscadorUsuarios(Model model) {
 		
+		List<UsuarioModel> listaUsuarios = usuarioRepo.findAll();
+		model.addAttribute("lista_usuarios", listaUsuarios);
 		return "template_buscador_usuarios";
 	}
 
