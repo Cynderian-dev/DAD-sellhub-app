f653c6e7de2cace515380025dc0a3d0a1d58265e
sergioj97 <sergiojsancam@gmail.com>
2022-02-22 21:08:44 +0100

    Eliminado GetMapping de ClienteController

--- src/main/java/es/urjccode/controllers/ClienteController.java
+++ src/main/java/es/urjccode/controllers/ClienteController.java
@@ -7,12 +7,6 @@ import org.springframework.web.bind.annotation.GetMapping;
 @Controller
 public class ClienteController {
 	
-	@GetMapping("/cliente")
-	public String cliente(Model model) {
-		
-		model.addAttribute("name", "cliente");
-		
-		return "cliente";
-	}
+	
 
 }
