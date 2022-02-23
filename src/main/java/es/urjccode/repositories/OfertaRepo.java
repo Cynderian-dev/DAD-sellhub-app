610d171792e6f49455fe7f5d8581c0807717e49f
sergioj97 <sergiojsancam@gmail.com>
2022-02-23 21:24:10 +0100

    Implementada búsqueda de ofertas por categorías

--- src/main/java/es/urjccode/repositories/OfertaRepo.java
+++ src/main/java/es/urjccode/repositories/OfertaRepo.java
@@ -4,6 +4,7 @@ import java.util.List;
 
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import es.urjccode.EnumCategorias;
 import es.urjccode.models.ListaModel;
 import es.urjccode.models.OfertaModel;
 import es.urjccode.models.UsuarioModel;
@@ -12,10 +13,16 @@ public interface OfertaRepo extends JpaRepository<OfertaModel, Long>{
 	
 	List<OfertaModel> findByUsuarioCreador(UsuarioModel usuarioCreador);
 	
+	List<OfertaModel> findByFechaCierreNullOrderByPrecio();
+	
 	List<OfertaModel> findByFechaCierreNullAndTituloContainingIgnoreCaseOrderByPrecio(String texto);
 	
 	List<OfertaModel> findByFechaCierreNullAndTituloContainingIgnoreCaseAndPrecioLessThanOrderByPrecio(String texto, double precioMaximo);
 
-	List<OfertaModel> findByFechaCierreNullOrderByPrecio();
+	List<OfertaModel> findByFechaCierreNullAndTituloContainingIgnoreCaseAndCategoriaEqualsOrderByPrecio(String texto,
+			EnumCategorias categoria);
+
+	List<OfertaModel> findByFechaCierreNullAndTituloContainingIgnoreCaseAndPrecioLessThanAndCategoriaEqualsOrderByPrecio(
+			String texto, double precioMaximo, EnumCategorias categoria);
 
 }
