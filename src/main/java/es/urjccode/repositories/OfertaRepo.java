d1e1c57c2a964328a9f02dc50e33c461c2348b23
sergioj97 <sergiojsancam@gmail.com>
2022-02-23 12:26:13 +0100

    Modificado OfertaRepo

--- src/main/java/es/urjccode/repositories/OfertaRepo.java
+++ src/main/java/es/urjccode/repositories/OfertaRepo.java
@@ -1,9 +1,15 @@
 package es.urjccode.repositories;
 
+import java.util.List;
+
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import es.urjccode.models.ListaModel;
 import es.urjccode.models.OfertaModel;
+import es.urjccode.models.UsuarioModel;
 
 public interface OfertaRepo extends JpaRepository<OfertaModel, Long>{
+	
+	List<OfertaModel> findByUsuarioCreador(UsuarioModel usuarioCreador);
 
 }
